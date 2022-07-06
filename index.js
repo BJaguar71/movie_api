a5b4f8ca71444103fc9062034f33daf44c2d1f69
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-06 13:01:05 +0200

    updated the key name of the movie objects

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 // defining the list of movies 
 let movies = [
     {
-        movie: 'The Matrix',
+        title: 'The Matrix',
         name: 'the Wachowskis',
         year: 1999
     },
     {
-        movie: 'The Matrix Reloaded',
+        title: 'The Matrix Reloaded',
         name: 'the Wachowskis',
         year: 2003
     },
     {
-        movie: 'The Matrix Revolutions',
+        title: 'The Matrix Revolutions',
         name: 'the Wachowskis',
         year: 2003,
     },
     {
-        movie: 'The Matrix Resurrections',
+        title: 'The Matrix Resurrections',
         name: 'Lena  Wachowski',
         year: 2021
     },
     {
-        movie: 'Bound',
+        title: 'Bound',
         name: 'the Wachowskis',
         year: 1996
     },
     {
-        movie: 'Speed Racer',
+        title: 'Speed Racer',
         name: 'the Wachowskis',
         year: 2008
     },
     {
-        movie: 'Sense8 (Series)',
+        title: 'Sense8 (Series)',
         name: 'the Wachowskis',
         year: '2015-2018'
     },
     {
-        movie: 'Cloud Atlas',
+        title: 'Cloud Atlas',
         name: 'the Wachowskis',
         year: 2012
     },
     {
-        movie: 'Jupiter Ascending',
+        title: 'Jupiter Ascending',
         name: 'the Wachowskis',
         year: 2015
     },
     {
-        movie: 'Pose (Series)',
+        title: 'Pose (Series)',
         name: [
             'Michaela Jaé Rodriguez',
             'Indya Moore',
@@ -57,7 +57,7 @@ let movies = [
         year: '2018-2021'
     },
     {
-        movie: 'Paris Is Burning (Documentary)',
+        title: 'Paris Is Burning (Documentary)',
         name: [
             'Dorian Cory',
             'Venus Xtravaganza',
@@ -67,97 +67,97 @@ let movies = [
         year: 1990
     },
     {
-        movie: 'The Umbrella Academy (Series)',
+        title: 'The Umbrella Academy (Series)',
         name: 'Elliot Page',
         year: '2019-present'
     },
     {
-        movie: 'Laverne Cox Presents: The T Word',
+        title: 'Laverne Cox Presents: The T Word',
         name: 'Laverne Cox',
         year: 2014
     },
     {
-        movie: 'Grandma',
+        title: 'Grandma',
         name: 'Laverne Cox',
         year: 2015
     },
     {
-        movie: 'Freak Show',
+        title: 'Freak Show',
         name: 'Laverne Cox',
         year: 2017
     },
     {
-        movie: 'Can You Keep a Secret?',
+        title: 'Can You Keep a Secret?',
         name: 'Lav,erne Cox',
         year: 2019
     },
     {
-        movie: 'Charlie\'s Angels',
+        title: 'Charlie\'s Angels',
         name: 'Laverne Cox',
         year: 2019
     },
     {
-        movie: 'Bad Hair',
+        title: 'Bad Hair',
         name: 'Laverne Cox',
         year: 2020
     },
     {
-        movie: 'Promising Young Woman',
+        title: 'Promising Young Woman',
         name:'Laverne Cox',
         year: 2020
     },
     {
-        movie: 'Disclosure: Trans Lives on Screen (Documentary)',
+        title: 'Disclosure: Trans Lives on Screen (Documentary)',
         name: 'Laverne Cox',
         year: 2020
     },
     {
-        movie: 'Jolt',
+        title: 'Jolt',
         name: 'Laverne Cox',
         year: 2021
     },
     {
-        movie: 'Grand Street',
+        title: 'Grand Street',
         name: 'Laverne Cox',
         year: 2014
     },
     {
-        movie: 'The Exhibitionists',
+        title: 'The Exhibitionists',
         name: 'Laverne Cox',
         year: 2012
     },
     {
-        movie: '36 Saints',
+        title: '36 Saints',
         name: 'Laverne Cox',
         year: 2013
     },
     {
-        movie: 'Carl[a]',
+        title: 'Carl[a]',
         name: 'Laverne Cox',
         year: 2011
     },
     {
-        movie: 'Musical Chairs',
+        title: 'Musical Chairs',
         name: 'Loverne Cox',
         year: 2011
     },
     {
-        movie: 'Bronox Paradise',
+        title: 'Bronox Paradise',
         name: 'Loverne Cox',
         year: 2010
     },
     {
-        movie: 'Uncle Stephanie',
+        title: 'Uncle Stephanie',
         name: 'Laverne Cox',
         year: 2009
     },
     {
-        movie: 'The King of Brooklyn',
+        title: 'The King of Brooklyn',
         name: 'Laverne Cox',
         year: 2004
     },
     {
-        movie: 'Orange Is the New Black (Series)',
+        title: 'Orange Is the New Black (Series)',
         name: 'Laverne Cox',
         year: '2013-2019'
     }
