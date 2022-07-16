cbc9b45bf6d426465a34babaf645215d2a5cb27a
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-16 19:35:43 +0200

    Updated the DELETE request to remove a movie from a user's list with mongoose query

--- index.js
+++ index.js
@@ -143,17 +143,19 @@ app.post('/users/:Username/movies/:MovieID', (req, res) => {
 });
 
 // Delete a movie from the user's favoriteMovies list
-app.delete('/users/:id/favoriteMovies/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-
-    let user = users.find(user => user.id == id);
-
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been deleted from user ${id}'s array.`);
-    } else {
-        res.status(400).send('user not found!')
-    }
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $pull: { FavoriteMovies: req.params.MovieID}
+    },
+    {new: true}, 
+    (err, updatedUser) => {
+        if(err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
 
 // Delete a user from the users's array
