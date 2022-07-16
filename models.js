e31292f53168b755ade9b67f4970a04c02c7e2f6
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-16 19:39:13 +0200

    defined schema for the movies collection

--- models.js
+++ models.js
@@ -1,3 +1,20 @@
 // importing mongoose
 const mongoose = require('mongoose');
 
+// defining schema for movies collection
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Summary: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    Image: String,
+    Featured: Boolean
+});
+
