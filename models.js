c80fab667ccff5e21e974239415709373b65c001
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-22 16:02:05 +0200

    corrected the typo

--- models.js
+++ models.js
@@ -20,7 +20,7 @@ let movieSchema = mongoose.Schema({
 
 // defining schema for users collection
 let userSchema = mongoose.Schema({
-    Userame: {type: String, required: true},
+    Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthdate: Date,
