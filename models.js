7ccf0070c4a209cca741cf51d17431aaa56057d3
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-22 16:03:54 +0200

    removed wrong codes
    I defined a separate schema for subdocs genre and director

--- models.js
+++ models.js
@@ -27,26 +27,10 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-// defining schema for genre subdoc
-let genreSchema = mongoose.Schema({
-    Name: {type: String, required: true},
-    Description: {type: String, required: true}
-});
-
-// defining schema for director subdoc
-let directorSchema = mongoose.Schema({
-    Name: {type: String, required: true},
-    Bio: {type: String, required: true}
-});
-
 // creating models
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
-let Genre = mongoose.model('Genre', genreSchema);
-let Director = mongoose.model('Director', directorSchema);
 
 // exporting the created models
 module.exports.Movie = Movie;
 module.exports.User = User;
-module.exports.Genre = Genre;
-module.exports.Directoe = Director;
