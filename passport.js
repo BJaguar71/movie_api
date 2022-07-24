42744b13237842ce2042c4e910d4a90eef6e367f
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-24 21:30:29 +0200

    defined passport-local strategy in order to define the basic HTTP authentication for login requests

--- passport.js
+++ passport.js
@@ -7,5 +7,24 @@ const passport =  require('passport'),
 let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
     ExtractJWT = passportJWT.ExtractJwt;
+// defining passport local strategy to define the basic HTTP authentication for login requests
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+}, (username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username or password.'});
+        }
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
 
     
