0ab2fe08d5da2921a88203723d1cd87fc1417d37
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-24 21:22:09 +0200

    imported passport, passport-local, passportJWT
    the authentication strategies

---
+++ passport.js
@@ -0,0 +1,10 @@
+// Importing passport, passport-local strategy, passportJWT and connecting the file to the models.js
+const passport =  require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
