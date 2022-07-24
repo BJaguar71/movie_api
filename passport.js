42369725dd2d8ed27152fd0d3db500c7f5a0c8ab
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-24 21:28:44 +0200

    updated the passport.js

--- passport.js
+++ passport.js
@@ -8,36 +8,4 @@ let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
     ExtractJWT = passportJWT.ExtractJwt;
 
-// defining passport local strategy to define the basic HTTP authentication for login requests
-passport.use(new LocalStrategy({
-    usernameField: 'Username',
-    passwordField: 'Password'
-}, (username, password, callback) => {
-    console.log(username + '  ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-            console.log(error);
-            return callback(error);
-        }
-        if (!user) {
-            console.log('incorrect username');
-            return callback(null, false, {message: 'Incorrect username or password.'});
-        }
-        console.log('finished');
-        return callback(null, user);
-    });
-}));
-
-// Setting up the JWT authentication-JWTStrategy
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret' // verifying the signature
-}, (jwtPayload, callback) => {
-    return Users.findById(jwtPayload._id)
-    .then((user) => {
-        return callback(null, user);
-    })
-    .catch((error) => {
-        return callback(error)
-    });
-}));
+    
