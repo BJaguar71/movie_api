80b4f51be750874a7d971fd654c4a84225635407
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-24 21:24:03 +0200

    imported JWT and passport

--- auth.js
+++ auth.js
@@ -1,3 +1,7 @@
 // Defining the authentication key, this has to be the same key used in the JWTStrategy
 const jwtSecret = 'your_jwt_secret';
 
+// Importing JSON Web Token and passport
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
