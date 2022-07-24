07a4fb3e3ca2cc26cd436f38ccfe07176a03747a
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-24 21:34:19 +0200

    defined the login endpoint for registered users that contains logic for authenticating users with basic HTTP authentication and generating a JWT token for authenticating future requests

--- auth.js
+++ auth.js
@@ -17,3 +17,23 @@ let generateJWTToken = (user) => {
     });
 }
 
+/* POST login. */
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false}, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
+            }
+            req.login(user, { session: false}, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token}); // It returns the token
+            });
+        })(req, res);
+    });
+}
