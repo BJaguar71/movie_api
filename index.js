2290a9490b344bae9b2d63ef343d29f3f7d798cb
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-27 19:25:39 +0200

    updated the listening port function

--- index.js
+++ index.js
@@ -232,6 +232,7 @@ app.use((err, req, res, next) => {
 });
 
 // app port listening
-app.listen(8080, () => {
-    console.log('listening on 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+    console.log('listening on Port ' + port);
 });
