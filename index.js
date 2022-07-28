52c2bb0d414ffcfca65feebe44ad31b5306fe8e9
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-28 14:08:54 +0200

    Revert "updated the code related to the listening port"
    This reverts commit a89531ee59b1560c9fc5b62a38042f1b1b42ed0c.

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ app.use((err, req, res, next) => {
 });
 
 // app port listening
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('listening on Port ' + PORT);
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+    console.log('listening on Port ' + port);
 });
