e9ea1d04ef177e26063bcc0e6da340f0a37ce8a5
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-07-28 13:33:54 +0200

    defined the connection to the online database

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 /*
 mongoose.connect('mongodb://localhost:27017/t-flix', { useNewUrlParser: true, useUnifiedTopology: true })
 */
+// connecting to the database
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const Models = require('./models.js');
 // importing body-parser
