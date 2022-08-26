e92a4e1fe1bfcc20828892928adc01603f083bee
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-26 12:24:41 +0200

    added auth midlware again

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.get('/', (req, res) => {
  *            content:
  *               application/json
  */
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
