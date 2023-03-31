1fcddc7bfb1f6059610fd91ab0deb2bc7ca0965e
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-31 10:39:00 +0200

    added another localhostport to the allowed origins

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const cors = require('cors');
 let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
+  "http://localhost:4200",
   "https://myflix-client-side-react.netlify.app"
 ]
 app.use(cors({
